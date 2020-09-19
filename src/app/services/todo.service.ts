90042a39c4f8e4162ce7ae018c7b3093f21dea71
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2020-09-19 10:28:21 +0430

    Facade respond

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Todo } from '../models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -19,24 +20,34 @@ export class TodoService {
     "completed": false
     */
 
-    formData.append('UserId', Math.random().toString().substring(1,3));
+    formData.append('UserId', Math.random().toString().substring(1, 3));
     formData.append('title', title);
     formData.append('completed', 'false');
-    return this.httpClient.post<Todo>(url, formData);
+    return this.httpClient.post<Todo>(url, formData).pipe(
+      map((respond) => {
+        return {
+          id: respond.id,
+          userId: Number(formData.get('UserId')),
+          title: String(formData.get('title')?.toString()).toString(),
+          listId: 1,
+          completed: Boolean(formData.get('completed')),
+        };
+      })
+    );
   }
 
   editTodoTitle(todoId: number, title: string): Observable<Todo> {
     const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
     const formData = new FormData();
     formData.append('title', title);
-    return this.httpClient.patch<Todo>(url,formData);
+    return this.httpClient.patch<Todo>(url, formData);
   }
 
   toggleTodo(todoId: number, completed: boolean) {
     const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
     const formData = new FormData();
     formData.append('completed', String(completed));
-    return this.httpClient.patch(url,formData);
+    return this.httpClient.patch(url, formData);
   }
 
   deleteTodo(todoId: number) {
@@ -46,7 +57,7 @@ export class TodoService {
 
   getAllTodo(): Observable<Todo[]> {
     const url = 'https://jsonplaceholder.typicode.com/todos';
-    return this.httpClient.get(url);
+    return this.httpClient.get<Todo[]>(url);
   }
 
   getTodo(todoId: number) {
