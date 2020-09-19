466a50bb04737ab3d9a727f87e2822274d4d34c8
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2020-09-19 11:05:06 +0430

    implement todo service

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../models';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  constructor(private httpClient: HttpClient) {}
+
+  createTodo(title: string): Observable<Todo> {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    const formData = new FormData();
+    /**
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+    */
+
+    formData.append('UserId', Math.random().toString().substring(1, 3));
+    formData.append('title', title);
+    formData.append('completed', 'false');
+    return this.httpClient.post<Todo>(url, formData).pipe(
+      map((respond) => {
+        return {
+          id: respond.id,
+          userId: Number(formData.get('UserId')),
+          title: String(formData.get('title')?.toString()).toString(),
+          listId: 1,
+          completed: Boolean(formData.get('completed')),
+        };
+      })
+    );
+  }
+
+  editTodoTitle(todoId: number, title: string): Observable<Todo> {
+    const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
+    const formData = new FormData();
+    formData.append('title', title);
+    return this.httpClient.patch<Todo>(url, formData);
+  }
+
+  toggleTodo(todoId: number, completed: boolean) {
+    const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
+    const formData = new FormData();
+    formData.append('completed', String(completed));
+    return this.httpClient.patch(url, formData);
+  }
+
+  deleteTodo(todoId: number) {
+    const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
+    return this.httpClient.delete(url);
+  }
+
+  getAllTodo(): Observable<Todo[]> {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    return this.httpClient.get<Todo[]>(url);
+  }
+
+  getTodo(todoId: number) {
+    const url = 'https://jsonplaceholder.typicode.com/todos/' + todoId;
+    return this.httpClient.get<Todo[]>(url);
+  }
+}
