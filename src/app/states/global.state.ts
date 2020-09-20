95ddd6cfb0b41fdecd3169805e3772fd826ca008
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2020-09-20 17:41:19 +0430

    Add CreateTodo

--- src/app/states/global.state.ts
+++ src/app/states/global.state.ts
@@ -1,11 +1,12 @@
 import { InjectionToken } from '@angular/core';
 import { RxState } from '@rx-angular/state';
-import { Todo } from '../models';
+import { List, Todo } from '../models';
 
 
 
 export interface GlobalState {
   todos: Todo[];
+  lists: List[];
 }
 
 export const GLOBAL_RX_STATE = new InjectionToken<RxState<GlobalState>>(
--- src/app/todos/pages/todo/todo.component.ts
+++ src/app/todos/pages/todo/todo.component.ts
@@ -1,13 +1,17 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RxState } from '@rx-angular/state';
-import { Todo } from 'src/app/models';
+import { Todo, List } from 'src/app/models';
 import { TodoService } from 'src/app/services/todo.service';
 import { GlobalState, GLOBAL_RX_STATE } from 'src/app/states/global.state';
+import { Subject } from 'rxjs';
+import { global } from '@angular/compiler/src/util';
 
 export interface TodoState {
   todos: Todo[];
+  lists: List[];
 }
 @Component({
   templateUrl: './todo.component.html',
@@ -16,6 +20,7 @@ export interface TodoState {
 export class TodoComponent extends RxState<TodoState> implements OnInit {
   todoFormGroup: FormGroup;
   readonly todos$ = this.select('todos');
+  newTodoBtn$ = new Subject();
 
   constructor(
     @Inject(GLOBAL_RX_STATE) private globalState: RxState<GlobalState>,
@@ -28,18 +33,33 @@ export class TodoComponent extends RxState<TodoState> implements OnInit {
     });
     this.connect('todos', this.globalState.select('todos'));
     // this.connect('todos', this.todoService.getAllTodo());
+    // this.connect(this.newTodoBtn$, (s, e) => ({ todos: s.todos.push({id:1,completed:false,title:'tello',userId:1}) }));
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onNewTodo(): void {
     if (this.todoFormGroup.controls.todoFormControl.value?.length > 0) {
-      // this.todos.unshift({
-      //   id: ++this.todos[0].id,
-      //   title: this.todoFormGroup.controls.todoFormControl.value,
-      //   completed: false,
-      // });
+      const todoTitle = this.todoFormGroup.controls.todoFormControl.value;
+      this.todoService.createTodo(todoTitle).subscribe(
+        (result) => {
+          const newTodo = {
+            id: result.id,
+            completed: false,
+            title: todoTitle,
+            userId: 1,
+          };
+          const newGlobalTodo = this.globalState.get('todos');
+          newGlobalTodo.unshift(newTodo);
+          this.globalState.set({
+            todos: newGlobalTodo,
+            lists: [{ id: 1, title: 'hello' }],
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
       this.todoFormGroup.controls.todoFormControl.setValue('');
     } else {
       this.snackbar.open('You must enter at leaset 1 Character', 'Sure!', {
