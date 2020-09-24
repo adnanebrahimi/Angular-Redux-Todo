4757e2fc5309e7dca07a91c9f7ce16fa65d39830
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2020-09-24 13:17:48 +0330

    refactor

--- src/app/store/todo.reducers.ts
+++ src/app/store/todo.reducers.ts
@@ -1,16 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
-import {
-  AddTodo,
-  getAllTodo,
-  getAllTodoFailure,
-  getAllTodoSuccess,
-} from './todo.actions';
+import { AddTodo, getAllTodo } from './todo.actions';
+import { Todo } from './todo.state';
 
-export interface State {}
+export interface State {
+  todos: Todo[];
+}
 
-const initialState: State = {};
+const initialState: State = {todos : []};
 
 export const reducer = createReducer(
   initialState,
-  on(getAllTodo, (state) => ({ ...state }))
+  on(getAllTodo),
+  on(AddTodo, (state, {title}) => ({
+    ...state,
+    todos: [...state.todos, {title, id: 1, completed: false}]
+  }))
 );
